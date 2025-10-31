ba314d544d0799e77bcc82873434636d01f2ddb1
AYOUB <ayoub.ardem@gmail.com>
2025-10-31 17:16:36 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -2,4 +2,19 @@ SELECT *
 FROM Booking B
 JOIN Property P ON B.property_id = P.property_id
 JOIN User U ON B.user_id = U.user_id
-JOIN Payment Pay ON B.booking_id = Pay.booking_id;
+JOIN Payment Pay ON B.booking_id = Pay.booking_id
+WHERE B.status = 'confirmed'
+  AND P.location = 'Casablanca';
+ 
+
+EXPLAIN ANALYZE
+SELECT 
+    B.booking_id, B.start_date, B.status,
+    U.name, P.title, P.location,
+    Pay.amount, Pay.status
+FROM Booking B
+JOIN Property P ON B.property_id = P.property_id
+JOIN User U ON B.user_id = U.user_id
+JOIN Payment Pay ON B.booking_id = Pay.booking_id
+WHERE B.status = 'confirmed'
+  AND P.location = 'Casablanca';
